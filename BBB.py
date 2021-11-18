6601cfbcaa7e75a002a466a04760248e7104279c
Спарки <cparkygreshin@gmail.com>
2021-11-18 10:32:29 +0300

    u

--- BBB.py
+++ BBB.py
@@ -1,6 +1,18 @@
-from module import*
+﻿from module import*
 users=["Vadim"]
 passwords=["321321"]
 
 password=passauto()
+
 print(password)
+while True:
+    print("Reg - 1, Avt - 2, Välja - 3")
+    v=int(input())
+    if v==1:
+        v=input("Sisestage nimi")
+    elif v==2:
+        v=input("Sisesta salasõna")
+    elif v==3:
+        print("Välja")
+    else:
+        print("On vaja valida 1,2 või 3.")
--- module.py
+++ module.py
@@ -13,4 +13,20 @@ def passauto()->str:
     print(ls)
     psword = ''.join([random.choice(ls) for x in range(12)])
     return(psword)
-
+def passcontrol(psword)->str:
+    psword=list(psword)
+    for i in psword:
+        if i.isdigit()== True:
+            digit="True"
+        if i.isalpha()== True:
+            alpha="True"
+        if i.isupper()== True:
+            upper="True"
+        if i.islower()== True:
+            lower="True"
+        if i in [".","_","/","@"]:
+            synbl="True"
+    if digit=="True" and upper=="True" and alpha=="True" and lower=="True" and synbl=="True":
+        ctr=True 
+    else:
+        ctr=False
